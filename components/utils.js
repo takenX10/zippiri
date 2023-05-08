051d99c7b7004ec2d1b276a16a31bed81198f1c8
takenX10 <taken@pop-os.localdomain>
2023-05-08 19:32:12 +0200

    generating logs for differences between saves

--- components/utils.js
+++ components/utils.js
@@ -1,6 +1,42 @@
 import { FileSystem, Dirs } from "react-native-file-access";
 import { zip } from 'react-native-zip-archive';
+import { encode} from 'base-64'
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+async function generate_stats(path){
+    const ls = await FileSystem.statDir(path)
+    let stats = {}
+    for(let f of ls){
+        if (f.type=="directory"){
+            stats[f.filename] = {value: await generate_stats(f.path), type:"directory" }
+        }else{
+            stats[f.filename] = {type:"file", value: await FileSystem.hash(f.path, 'SHA-256')}
+        }
+    }
+    return stats
+}
+
+// deleted -> everything that's in source and not in target
+// added -> everything that's in target and not in source
+function compare_stats(before_stats, after_stats){
+    let final_stats = {added:{}, deleted:{}, files:after_stats}
+    for(let file in before_stats){
+        if(after_stats[file] != before_stats[file]){
+            if(after_stats[file]["type"] == "directory"){
+                compare_stats() // TODO: FINISH THIS
+            }else{
+                final_stats["deleted"][file] = before_stats[file]
+            }
+        }
+    }
+    for(let files in after_stats){
+        if(after_stats[files] != before_stats[files]){
+            final_stats["added"][files] = after_stats[files]
+        }
+    }
+    return final_stats
+}
 
 async function get_server_url() {
     let url = await AsyncStorage.getItem("address")
@@ -74,7 +110,6 @@ async function delete_folder(path) {
 }
 
 async function copy_directory(source, target) {
-    console.log(`COPYING ${source} ${target}`)
     if (await FileSystem.exists(target)) {
         await delete_folder(target)
     }
@@ -92,14 +127,14 @@ async function copy_directory(source, target) {
 async function upload_full(path) {
     try {
         const c = await login()
+        let signature = await AsyncStorage.getItem("signature")
+        signature = signature.substring(1, signature.length - 1)
         let d = new Date();
         d = d.toISOString()
         const date = d.substring(0, d.length - 2);
         let n = path.split("%3A");
         const name = n[n.length - 1]
-        console.log(date, name)
         let status = await start_upload(name, "full", date, c)
-        console.log(status)
         if (!status) {
             return false;
         }
@@ -110,10 +145,15 @@ async function upload_full(path) {
             await delete_folder(`${Dirs.DocumentDir}/full/${date}`)
         }
         await FileSystem.mkdir(`${Dirs.DocumentDir}/full/${date}`)
-
-        console.log("made the directory")
+        console.log("GENSTATUS")
+        const stats = await generate_stats(path)
+        console.log(stats)
+        let base64_stats = encode(JSON.stringify(stats))
+        let upload_stats_res = await upload(name, "full", date, base64_stats, `.${signature}`, c)
+        if (!upload_stats_res) {
+            return false;
+        }
         await copy_directory(path, `${Dirs.DocumentDir}/full/${date}/tmp`)
-        console.log("Directory copied")
         await zip(`${Dirs.DocumentDir}/full/${date}/tmp`, `${Dirs.DocumentDir}/full/${date}/${date}.zip`)
         let f = await FileSystem.readFile(`${Dirs.DocumentDir}/full/${date}/${date}.zip`, 'base64')
         let res_upload = await upload(name, "full", date, f, `${date}.zip`, c)
